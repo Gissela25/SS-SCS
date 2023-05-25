af7307d16865c11b289d678d0a25e0ce7d50c1ba
Gissela Serrano <55642333+Gissela25@users.noreply.github.com>
2023-05-24 18:50:37 -0600

    FixingBugs

--- Models/ArticlesModel.php
+++ Models/ArticlesModel.php
@@ -22,7 +22,7 @@ class ArticlesModel extends ConnectionModel{
         }
         else{
             //En caso de que la variable no esté vacía, cremos la consulta utilizando WHERE para indicar el registro que traeremos
-            $query = "SELECT articulos.Id_Articulo, articulos.NombreA, presentaciones.NombreP, areas.Nombre, departamentos.NombreD, articulos.Id_Estado FROM articulos JOIN presentaciones ON articulos.Id_Presentacion = presentaciones.Id_Presentacion JOIN areas ON articulos.Id_Area = areas.Id_Area JOIN departamentos ON articulos.Id_Departamento = departamentos.Id_Departamento WHERE Id_Articulo=:Id_Articulo";
+            $query = "SELECT articulos.Id_Articulo, articulos.NombreA, presentaciones.NombreP, areas.Nombre, departamentos.NombreD articulos.Id_Estado FROM articulos JOIN presentaciones ON articulos.Id_Presentacion = presentaciones.Id_Presentacion JOIN areas ON articulos.Id_Area = areas.Id_Area JOIN departamentos ON articulos.Id_Departamento = departamentos.Id_Departamento WHERE Id_Articulo=:Id_Articulo";
             //Retornamos el registro
             return $this->get_query($query,[":Id_Articulo"=>$id]);
         }
@@ -32,7 +32,7 @@ class ArticlesModel extends ConnectionModel{
     {
        
         //Creamos la consulta para ingresar los datos
-        $query = "INSERT INTO articulos(Id_Articulo, NombreA , Id_Presentacion, Id_Departamento, Id_Area ) VALUES(  :Id_Articulo, :NombreA, :Id_Presentacion, :Id_Departamento  ,:Id_Area )";
+        $query = "INSERT INTO articulos(Id_Articulo, NombreA , Id_Presentacion, Id_Departamento, Id_Area) VALUES(  :Id_Articulo, :NombreA, :Id_Presentacion, :Id_Departamento  ,:Id_Area)";
         //Utilzamos el método set_query para realizar un registro
         $result = $this->set_query($query,$arreglo);
 
