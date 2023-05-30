777c1076099ec0d3af0b09b9104913f645dc4a0d
susan749 <60519933+susan749@users.noreply.github.com>
2023-05-29 23:56:34 -0600

    Update

--- Views/Movements/EntryDate.php
+++ Views/Movements/EntryDate.php
@@ -11,7 +11,7 @@ include_once "./Core/config.php"
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
-    <title>Document</title>
+    <title>Entradas por rango de fecha</title>
     <link rel="stylesheet" href="styles/style.css">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/css/bootstrap.min.css">
@@ -26,7 +26,7 @@ include_once "./Core/config.php"
  require_once "./Views/NavbarScreen.php";
 ?>
     <div class="row mx-5 mt-5">
-        <h2 style="text-align:center">Entradas</h5>
+        <h2 style="text-align:center">Entradas por rango de fecha</h5>
             <h3>Centro de Sangre: <?=$_SESSION['dataBuffer']['Nombre']?> <?=$_SESSION['dataBuffer']['Apellido']?></h3>
             <div class="col ml-5">
                 <div class="row mt-3">
--- Views/Movements/EntryDateByDate.php
+++ Views/Movements/EntryDateByDate.php
@@ -11,7 +11,7 @@ include_once "./Core/config.php"
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
-    <title>Kardex por artículo - Cruz Roja</title>
+    <title>Ingreso por rango de fecha</title>
     <link rel="stylesheet" href="styles/style.css">
 
     <!--
