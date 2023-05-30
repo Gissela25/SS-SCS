a6ff7d404d82b1a0947caa2197777abca4aa8d3b
susan749 <60519933+susan749@users.noreply.github.com>
2023-05-29 23:13:48 -0600

    update

--- Views/Movements/MovementsByDeparmentReport.php
+++ Views/Movements/MovementsByDeparmentReport.php
@@ -11,7 +11,7 @@ ob_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <title>Reporte kardex por Articulo</title>
+    <title>Reporte movimientos por departamento</title>
     <style>
     body {
         margin: 0 auto;
