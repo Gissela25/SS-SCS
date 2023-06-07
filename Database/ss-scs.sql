c8d28233c0165d2ab28fe302ddd98a30eb35f93d
Gissela Serrano <55642333+Gissela25@users.noreply.github.com>
2023-06-07 11:38:08 -0600

    DBDONE

--- Database/ss-scs.sql
+++ Database/ss-scs.sql
@@ -2,10 +2,10 @@
 -- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Jun 02, 2023 at 11:22 PM
--- Server version: 8.0.31
--- PHP Version: 8.0.26
+-- Servidor: 127.0.0.1:3306
+-- Tiempo de generación: 07-06-2023 a las 17:37:44
+-- Versión del servidor: 8.0.31
+-- Versión de PHP: 8.0.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,12 +18,12 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `ss-scs`
+-- Base de datos: `ss-scs`
 --
 
 DELIMITER $$
 --
--- Functions
+-- Funciones
 --
 DROP FUNCTION IF EXISTS `generar_codigo`$$
 CREATE DEFINER=`root`@`localhost` FUNCTION `generar_codigo` () RETURNS VARCHAR(6) CHARSET utf8mb3 COLLATE utf8mb3_unicode_ci  BEGIN
@@ -40,7 +40,7 @@ DELIMITER ;
 -- --------------------------------------------------------
 
 --
--- Table structure for table `areas`
+-- Estructura de tabla para la tabla `areas`
 --
 
 DROP TABLE IF EXISTS `areas`;
@@ -54,7 +54,7 @@ CREATE TABLE IF NOT EXISTS `areas` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
 --
--- Dumping data for table `areas`
+-- Volcado de datos para la tabla `areas`
 --
 
 INSERT INTO `areas` (`Id_Area`, `Nombre`, `Id_Estado`) VALUES
@@ -67,7 +67,7 @@ INSERT INTO `areas` (`Id_Area`, `Nombre`, `Id_Estado`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `articulos`
+-- Estructura de tabla para la tabla `articulos`
 --
 
 DROP TABLE IF EXISTS `articulos`;
@@ -88,10 +88,11 @@ CREATE TABLE IF NOT EXISTS `articulos` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
 --
--- Dumping data for table `articulos`
+-- Volcado de datos para la tabla `articulos`
 --
 
 INSERT INTO `articulos` (`Id_Articulo`, `Codigo`, `NombreA`, `Id_Presentacion`, `Id_Departamento`, `Id_Area`, `Id_Estado`) VALUES
+('I10626524267147', '123456789', 'Soda', 'P123', 'D75300', 'A12343', 1),
 ('I17444664310128', '20212129', 'Agua Mineral', 'P123', 'D32551', 'A12343', 1),
 ('I33172199242160', '202226023', 'Agua Deshidratada', 'P123', 'D96937', 'A59165', 1),
 ('I45995451110446', '20222699', 'Acetaminofen', 'P123', 'D44522', 'A12343', 1),
@@ -100,12 +101,13 @@ INSERT INTO `articulos` (`Id_Articulo`, `Codigo`, `NombreA`, `Id_Presentacion`, 
 ('I58872765406360', '20128999', 'Kit de tamizaje', 'P123', 'D44522', 'A12345', 1),
 ('I83045535656790', '202226056', 'Mascarilla', 'P123', 'D96937', 'A59165', 1),
 ('I89489943973588', '20129094', 'Prueba de Sifilis', 'P123', 'D44522', 'A12343', 1),
+('I95770845223092', '87979857', 'Mascarillas', 'P123', 'D44522', 'A12343', 1),
 ('I99745938237056', '20212127', 'Jeringas', 'P123', 'D44522', 'A12345', 1);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `correlativos`
+-- Estructura de tabla para la tabla `correlativos`
 --
 
 DROP TABLE IF EXISTS `correlativos`;
@@ -117,20 +119,26 @@ CREATE TABLE IF NOT EXISTS `correlativos` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
 --
--- Dumping data for table `correlativos`
+-- Volcado de datos para la tabla `correlativos`
 --
 
 INSERT INTO `correlativos` (`Id_Correlativo`, `Id_Usuario`) VALUES
+('202306071826', 'GS2367'),
+('202306075523', 'GS2367'),
+('202306076119', 'GS2367'),
 ('202305301432', 'JD0001'),
 ('202305304143', 'JD0001'),
 ('202305308191', 'JD0001'),
 ('202305309597', 'JD0001'),
-('202305309951', 'JD0001');
+('202305309951', 'JD0001'),
+('202306073922', 'JD0001'),
+('202306076916', 'JD0001'),
+('202306077860', 'JD0001');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `departamentos`
+-- Estructura de tabla para la tabla `departamentos`
 --
 
 DROP TABLE IF EXISTS `departamentos`;
@@ -144,18 +152,19 @@ CREATE TABLE IF NOT EXISTS `departamentos` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
 --
--- Dumping data for table `departamentos`
+-- Volcado de datos para la tabla `departamentos`
 --
 
 INSERT INTO `departamentos` (`Id_Departamento`, `NombreD`, `Id_Estado`) VALUES
-('D32551', 'Crecitin', 2),
-('D44522', 'Ambu', 1),
+('D32551', 'Almacen', 1),
+('D44522', 'Ambulancias', 1),
+('D75300', 'Sangrias', 1),
 ('D96937', 'Gerencia', 1);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `estados`
+-- Estructura de tabla para la tabla `estados`
 --
 
 DROP TABLE IF EXISTS `estados`;
@@ -166,7 +175,7 @@ CREATE TABLE IF NOT EXISTS `estados` (
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
 --
--- Dumping data for table `estados`
+-- Volcado de datos para la tabla `estados`
 --
 
 INSERT INTO `estados` (`Id_Estado`, `Estado`) VALUES
@@ -176,7 +185,7 @@ INSERT INTO `estados` (`Id_Estado`, `Estado`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `estados_movimiento`
+-- Estructura de tabla para la tabla `estados_movimiento`
 --
 
 DROP TABLE IF EXISTS `estados_movimiento`;
@@ -187,7 +196,7 @@ CREATE TABLE IF NOT EXISTS `estados_movimiento` (
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
 --
--- Dumping data for table `estados_movimiento`
+-- Volcado de datos para la tabla `estados_movimiento`
 --
 
 INSERT INTO `estados_movimiento` (`Id_EstadoMov`, `Estado_Movimiento`) VALUES
@@ -197,7 +206,7 @@ INSERT INTO `estados_movimiento` (`Id_EstadoMov`, `Estado_Movimiento`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `existencias`
+-- Estructura de tabla para la tabla `existencias`
 --
 
 DROP TABLE IF EXISTS `existencias`;
@@ -214,16 +223,18 @@ CREATE TABLE IF NOT EXISTS `existencias` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
 --
--- Dumping data for table `existencias`
+-- Volcado de datos para la tabla `existencias`
 --
 
 INSERT INTO `existencias` (`Id_Existencia`, `Id_Articulo`, `NoComprobante`, `Saldo`, `SaldoInicial`, `F_LastUpdate`, `EsSaldoInicial`) VALUES
-('E26724557571726', 'I89489943973588', '26202308', 60, 60, '2023-05-30', 0),
+('E26724557571726', 'I89489943973588', '26202308', 58, 60, '2023-05-30', 0),
 ('E33290568337510', 'I45995451110446', NULL, 0, NULL, '2023-06-02', 1),
-('E35578389916098', 'I58544007596640', '26202308', 12, 20, '2023-05-30', 0),
+('E35578389916098', 'I58544007596640', '23456789', 50, 20, '2023-06-07', 0),
+('E36742947971538', 'I95770845223092', '23456789', 40, 45, '2023-06-07', 0),
 ('E43321048868076', 'I99745938237056', '', 0, 25, '2023-05-24', 1),
 ('E57328323996558', 'I58872765406360', '26202308', 20, 5, '2023-05-27', 1),
 ('E62304667623815', 'I33172199242160', '26202308', 60, 60, '2023-05-27', 1),
+('E70638510411623', 'I10626524267147', NULL, 0, NULL, '2023-06-07', 1),
 ('E81797379711633', 'I17444664310128', '26202311', 5, 5, '2023-05-27', 1),
 ('E87038057115618', 'I83045535656790', '26202311', 5, 5, '2023-05-27', 1),
 ('E95265838413285', 'I48081567842105', '26202305', 30, 35, '2023-05-30', 0);
@@ -231,7 +242,7 @@ INSERT INTO `existencias` (`Id_Existencia`, `Id_Articulo`, `NoComprobante`, `Sal
 -- --------------------------------------------------------
 
 --
--- Table structure for table `movimientos`
+-- Estructura de tabla para la tabla `movimientos`
 --
 
 DROP TABLE IF EXISTS `movimientos`;
@@ -250,7 +261,7 @@ CREATE TABLE IF NOT EXISTS `movimientos` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
 --
--- Dumping data for table `movimientos`
+-- Volcado de datos para la tabla `movimientos`
 --
 
 INSERT INTO `movimientos` (`Id_Correlativo`, `Id_Articulo`, `Id_Existencia`, `Entrada`, `Salida`, `Correctivo`, `SaldoResultante`, `F_Movimiento`) VALUES
@@ -259,12 +270,19 @@ INSERT INTO `movimientos` (`Id_Correlativo`, `Id_Articulo`, `Id_Existencia`, `En
 ('202305309951', 'I89489943973588', 'E26724557571726', 60, 0, 0, 60, '2023-05-30'),
 ('202305309597', 'I48081567842105', 'E95265838413285', 35, 0, 0, 35, '2023-05-30'),
 ('202305308191', 'I58544007596640', 'E35578389916098', 0, 8, 0, 12, '2023-05-30'),
-('202305308191', 'I48081567842105', 'E95265838413285', 0, 5, 0, 30, '2023-05-30');
+('202305308191', 'I48081567842105', 'E95265838413285', 0, 5, 0, 30, '2023-05-30'),
+('202306077860', 'I58544007596640', 'E35578389916098', 56, 0, 0, 68, '2023-06-07'),
+('202306073922', 'I58544007596640', 'E35578389916098', 0, 0, 1, 69, '2023-06-07'),
+('202306076916', 'I89489943973588', 'E26724557571726', 0, 2, 0, 58, '2023-06-07'),
+('202306076119', 'I58544007596640', 'E35578389916098', 0, 9, 0, 60, '2023-06-07'),
+('202306071826', 'I95770845223092', 'E36742947971538', 45, 0, 0, 45, '2023-06-07'),
+('202306075523', 'I95770845223092', 'E36742947971538', 0, 5, 0, 40, '2023-06-07'),
+('202306075523', 'I58544007596640', 'E35578389916098', 0, 10, 0, 50, '2023-06-07');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `movimientos_temp`
+-- Estructura de tabla para la tabla `movimientos_temp`
 --
 
 DROP TABLE IF EXISTS `movimientos_temp`;
@@ -284,7 +302,7 @@ CREATE TABLE IF NOT EXISTS `movimientos_temp` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `presentaciones`
+-- Estructura de tabla para la tabla `presentaciones`
 --
 
 DROP TABLE IF EXISTS `presentaciones`;
@@ -298,17 +316,18 @@ CREATE TABLE IF NOT EXISTS `presentaciones` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
 --
--- Dumping data for table `presentaciones`
+-- Volcado de datos para la tabla `presentaciones`
 --
 
 INSERT INTO `presentaciones` (`Id_Presentacion`, `NombreP`, `Id_Estado`) VALUES
 ('P123', 'Unidad', 1),
-('P234', '250 ml', 2);
+('P234', '250 ml', 1),
+('P794', '569ml', 1);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tipos_usuario`
+-- Estructura de tabla para la tabla `tipos_usuario`
 --
 
 DROP TABLE IF EXISTS `tipos_usuario`;
@@ -319,7 +338,7 @@ CREATE TABLE IF NOT EXISTS `tipos_usuario` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
 --
--- Dumping data for table `tipos_usuario`
+-- Volcado de datos para la tabla `tipos_usuario`
 --
 
 INSERT INTO `tipos_usuario` (`Id_Tipo`, `Tipo`) VALUES
@@ -329,7 +348,7 @@ INSERT INTO `tipos_usuario` (`Id_Tipo`, `Tipo`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `usuarios`
+-- Estructura de tabla para la tabla `usuarios`
 --
 
 DROP TABLE IF EXISTS `usuarios`;
@@ -340,7 +359,7 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   `Correo` varchar(75) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
   `Clave` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
   `Tipo_Usuario` int NOT NULL DEFAULT '1',
-  `Id_Area` char(6) COLLATE utf8mb3_unicode_ci NOT NULL,
+  `Id_Area` char(6) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
   `Id_Estado` int NOT NULL DEFAULT '1',
   PRIMARY KEY (`Id_Usuario`),
   KEY `Id_Estado` (`Id_Estado`),
@@ -349,27 +368,28 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
 --
--- Dumping data for table `usuarios`
+-- Volcado de datos para la tabla `usuarios`
 --
 
 INSERT INTO `usuarios` (`Id_Usuario`, `Nombre`, `Apellido`, `Correo`, `Clave`, `Tipo_Usuario`, `Id_Area`, `Id_Estado`) VALUES
-('GS2367', 'Gissela', 'Serrano', 'gissela25serrano@gmail.com', 'ef797c8118f02dfb649607dd5d3f8c7623048c9c063d532cc95c5ed7a898a64f', 1, 'A48672', 1),
+('CM2329', 'Carlos', 'Lopez', 'carlosm@gmail.com', 'ef797c8118f02dfb649607dd5d3f8c7623048c9c063d532cc95c5ed7a898a64f', 1, 'A48672', 2),
+('GS2367', 'Gissela', 'Serrano', 'gissela25serrano@gmail.com', 'e24df920078c3dd4e7e8d2442f00e5c9ab2a231bb3918d65cc50906e49ecaef4', 1, 'A12343', 1),
 ('JD0001', 'John', 'Doe', 'johndoe@gmail.com', 'ef797c8118f02dfb649607dd5d3f8c7623048c9c063d532cc95c5ed7a898a64f', 0, 'A12343', 1),
 ('JM2366', 'Jony', 'Morales', 'jony25@gmail.com', 'ef797c8118f02dfb649607dd5d3f8c7623048c9c063d532cc95c5ed7a898a64f', 1, 'A48672', 1),
 ('SM2317', 'Santiago', 'Melendez', 'santiago@gmail.com', 'ef797c8118f02dfb649607dd5d3f8c7623048c9c063d532cc95c5ed7a898a64f', 1, 'A27215', 1);
 
 --
--- Constraints for dumped tables
+-- Restricciones para tablas volcadas
 --
 
 --
--- Constraints for table `areas`
+-- Filtros para la tabla `areas`
 --
 ALTER TABLE `areas`
   ADD CONSTRAINT `areas_ibfk_1` FOREIGN KEY (`Id_Estado`) REFERENCES `estados` (`Id_Estado`);
 
 --
--- Constraints for table `articulos`
+-- Filtros para la tabla `articulos`
 --
 ALTER TABLE `articulos`
   ADD CONSTRAINT `articulos_ibfk_1` FOREIGN KEY (`Id_Presentacion`) REFERENCES `presentaciones` (`Id_Presentacion`),
@@ -378,25 +398,25 @@ ALTER TABLE `articulos`
   ADD CONSTRAINT `articulos_ibfk_4` FOREIGN KEY (`Id_Estado`) REFERENCES `estados` (`Id_Estado`);
 
 --
--- Constraints for table `correlativos`
+-- Filtros para la tabla `correlativos`
 --
 ALTER TABLE `correlativos`
   ADD CONSTRAINT `correlativos_ibfk_1` FOREIGN KEY (`Id_Usuario`) REFERENCES `usuarios` (`Id_Usuario`);
 
 --
--- Constraints for table `departamentos`
+-- Filtros para la tabla `departamentos`
 --
 ALTER TABLE `departamentos`
   ADD CONSTRAINT `departamentos_ibfk_1` FOREIGN KEY (`Id_Estado`) REFERENCES `estados` (`Id_Estado`);
 
 --
--- Constraints for table `existencias`
+-- Filtros para la tabla `existencias`
 --
 ALTER TABLE `existencias`
   ADD CONSTRAINT `existencias_ibfk_1` FOREIGN KEY (`Id_Articulo`) REFERENCES `articulos` (`Id_Articulo`);
 
 --
--- Constraints for table `movimientos`
+-- Filtros para la tabla `movimientos`
 --
 ALTER TABLE `movimientos`
   ADD CONSTRAINT `movimientos_ibfk_1` FOREIGN KEY (`Id_Correlativo`) REFERENCES `correlativos` (`Id_Correlativo`),
@@ -404,7 +424,7 @@ ALTER TABLE `movimientos`
   ADD CONSTRAINT `movimientos_ibfk_3` FOREIGN KEY (`Id_Existencia`) REFERENCES `existencias` (`Id_Existencia`);
 
 --
--- Constraints for table `movimientos_temp`
+-- Filtros para la tabla `movimientos_temp`
 --
 ALTER TABLE `movimientos_temp`
   ADD CONSTRAINT `movimientos_temp_ibfk_1` FOREIGN KEY (`Id_EstadoMov`) REFERENCES `estados_movimiento` (`Id_EstadoMov`),
@@ -413,13 +433,13 @@ ALTER TABLE `movimientos_temp`
   ADD CONSTRAINT `movimientos_temp_ibfk_5` FOREIGN KEY (`Id_Existencia`) REFERENCES `existencias` (`Id_Existencia`);
 
 --
--- Constraints for table `presentaciones`
+-- Filtros para la tabla `presentaciones`
 --
 ALTER TABLE `presentaciones`
   ADD CONSTRAINT `presentaciones_ibfk_1` FOREIGN KEY (`Id_Estado`) REFERENCES `estados` (`Id_Estado`);
 
 --
--- Constraints for table `usuarios`
+-- Filtros para la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
   ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`Id_Estado`) REFERENCES `estados` (`Id_Estado`),
